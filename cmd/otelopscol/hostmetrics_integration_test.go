cc144e93d8ad9b4b34acb2214407973bd0383312
Martijn van Schaardenburg <martijnvs@google.com>
2022-09-30 16:41:24 -0400

    get something that looks like it should compile

--- cmd/otelopscol/hostmetrics_integration_test.go
+++ cmd/otelopscol/hostmetrics_integration_test.go
@@ -1,27 +1,25 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"testing"
+	"time"
+	"os"
 )
 
 func TestHostmetrics(t *testing.T) {
-	// Make a few tweaks to config-example.yaml
-	config := readConfig("config-example.yaml")
-	writeConfig(adjustConfig(config), "new-config.yaml")
-	flag.Set("config", "new-confg.yaml")
+	flag.Set("config", "config-for-testing.yaml")
+
+	ctx, cancel = context.WithTimeout(context.Background(), 8*time.Second)
+	defer cancel()
 
 	// Run the main function of otelopscol.
-	// TODO: set a 20 second timeout on the passed context.
 	mainContext(ctx)
 
-	// Example: https://paste.googleplex.com/5536046585479168
-	exportedData := readFile("metrics.json")
-	// This file will bear a strong resemblance to config_example.yaml
-	// from the ops-agent repo.
-	expectations := readFile("hostmetrics_expectations.yaml")
-
-	// Logic for applying the assertions in hostmetrics_expectations.yaml
-	// to the exported data in metrics.json.
-	expectDataLooksLike(exportedData, expectations)
+	data, err := os.ReadFile("metrics.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Fatal(string(data))
 }

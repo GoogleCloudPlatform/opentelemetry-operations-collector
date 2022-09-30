745f4aafe00af4c4422003d5306a733141b0ed13
Martijn van Schaardenburg <martijnvs@google.com>
2022-09-30 11:33:01 -0400

    convert to pseudocode

--- cmd/otelopscol/hostmetrics_integration_test.go
+++ cmd/otelopscol/hostmetrics_integration_test.go
@@ -1,15 +1,24 @@
-package main_test
-
 package main
 
 import (
-	"context"
+	"flag"
 	"testing"
-	"time"
 )
 
-func TestBasic(t *testing.T) {
-	ctx := context.WithTimeout(context.Background(), 10 * time.Second)
-	// Should fail with a 'no config' error
+func TestHostmetrics(t *testing.T) {
+	// Make a few tweaks to config-example.yaml
+	config := readConfig("config-example.yaml")
+	writeConfig(adjustConfig(config), "new-config.yaml")
+	flag.Set("config", "new-confg.yaml")
+
+	// Run the main function of otelopscol.
+	// TODO: set a 20 second timeout on the passed context.
 	mainContext(ctx)
+
+	exportedData := readFile("metrics.json")
+	expectations := readFile("hostmetrics_expectations.yaml")
+
+	// Logic for applying the assertions in hostmetrics_expectations.yaml
+	// to the exported data in metrics.json.
+	expectDataLooksLike(exportedData, expectations)
 }

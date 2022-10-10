7ac8f3ab049391f079e9522180c123a496ce595f
Martijn van Schaardenburg <martijnvs@google.com>
2022-10-10 20:01:41 +0000

    test is 99% passing!

--- cmd/otelopscol/hostmetrics_integration_test.go
+++ cmd/otelopscol/hostmetrics_integration_test.go
@@ -15,35 +15,217 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
 	"testing"
 	"time"
 
+	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/pmetric"
+	"gopkg.in/yaml.v2"
 )
 
 func TestHostmetrics(t *testing.T) {
-	terminationTime := 8*time.Second
+	terminationTime := 4 * time.Second
 	ctx, cancel := context.WithTimeout(context.Background(), terminationTime)
 	defer cancel()
 
-	os.Args = append(os.Args, "--config=config-for-testing.yaml")
+	testDir, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	os.Args = append(os.Args, fmt.Sprintf("--config=%s", filepath.Join(testDir, "config-for-testing.yaml")))
+
+	scratchDir, err := os.MkdirTemp("", "")
+	if err != nil {
+		t.Fatalf("Couldn't create scratch directory. err=%v", err)
+	}
+	os.Chdir(scratchDir)
 
 	// Run the main function of otelopscol.
 	// It will self-terminate in terminationTime.
 	mainContext(ctx)
 
-	data, err := os.ReadFile("metrics.json")
+	observed := loadObservedMetrics(t, scratchDir)
+	expected := loadExpectedMetrics(t, testDir)
+	expectMetricsLookRight(t, observed, expected)
+}
+
+func loadObservedMetrics(t *testing.T, scratchDir string) pmetric.Metrics {
+	metricsJSONPath := filepath.Join(scratchDir, "metrics.json")
+	data, err := os.ReadFile(metricsJSONPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("Found %v bytes of data at %s", len(data), metricsJSONPath)
+
+	lines := strings.Split(string(data), "\n")
+	if len(lines) < 2 {
+		t.Fatalf("Only found %v lines in %q, need at least 2", len(lines), metricsJSONPath)
+	}
+
+	// Take the second batch of data exported by otelopscol, because the
+	// first batch doesn't include any data for system.cpu.utilization.
+	secondBatch := lines[1]
+	metrics, err := pmetric.NewJSONUnmarshaler().UnmarshalMetrics([]byte(secondBatch))
+	if err != nil {
+		t.Fatal(err)
+	}
+	return metrics
+}
+
+func loadExpectedMetrics(t *testing.T, testDir string) map[string]ExpectedMetric {
+	data, err := os.ReadFile(filepath.Join(testDir, "expected-metrics.yaml"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	var agentMetrics struct {
+		ExpectedMetrics []ExpectedMetric `yaml:"expected_metrics" validate:"unique=Name,dive"`
+	}
+
+	err = yaml.UnmarshalStrict(data, &agentMetrics)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	metrics, err := pmetric.NewJSONUnmarshaler().UnmarshalMetrics(data)
-	if err != nil {
-		t.Fatal(err)
+	result := make(map[string]ExpectedMetric)
+	for _, expect := range agentMetrics.ExpectedMetrics {
+		result[expect.Name] = expect
 	}
-	scopeMetrics0 := metrics.ResourceMetrics().At(1).ScopeMetrics().At(0)
-	t.Error(scopeMetrics0.Scope().Name())
-	t.Error(scopeMetrics0.Metrics().At(0).Name())
-	t.Error(scopeMetrics0.Metrics().At(0).Unit())
+	return result
+}
+
+// ExpectedMetric encodes a series of assertions about what data we expect
+// to see in the metrics backend.
+type ExpectedMetric struct {
+	// The metric name, for example network/tcp_connections.
+	Name string `yaml:"name" validate:"required"`
+	// The value type, for example INT64.
+	// TODO: check the value type.
+	ValueType string `yaml:"value_type" validate:"required,oneof=BOOL INT64 DOUBLE STRING DISTRIBUTION"`
+	// The kind, for example GAUGE.
+	Kind string `yaml:"kind" validate:"required,oneof=GAUGE DELTA CUMULATIVE"`
+	// Mapping of expected label keys to value patterns.
+	// Patterns are RE2 regular expressions.
+	// TODO: check that the set of expected attributes is exhaustive?
+	Attributes map[string]string `yaml:"attributes,omitempty" validate:"omitempty,gt=0"`
+}
+
+// keys returns a slice containing just the keys from the input map m.
+func keys[K comparable, V any](m map[K]V) []K {
+	var ks []K
+	for k, _ := range m {
+		ks = append(ks, k)
+	}
+	return ks
+}
+
+func expectAttributesMatch(t *testing.T, dataPoints pmetric.NumberDataPointSlice, expectedAttrs map[string]string, metricName string) {
+	// Map from attribute to set of observed values that that attribute takes on.
+	// For example,
+	//   map[
+	//     cpu_state:map[idle:true interrupt:true]
+	//     pid:map[123:true 456:true]
+	//   ]
+	collectedAttributes := make(map[string]map[string]bool)
+
+	// Collect all the attributes across all data points into the
+	// `collectedAttributes` map.
+	for i := 0; i < dataPoints.Len(); i++ {
+		dataPoint := dataPoints.At(i)
+		attributes := dataPoint.Attributes()
+		attributes.Range(func(k string, v pcommon.Value) bool {
+			if _, ok := collectedAttributes[k]; !ok {
+				collectedAttributes[k] = make(map[string]bool)
+			}
+			collectedAttributes[k][v.AsString()] = true
+			return true // Tell Range() to keep going.
+		})
+	}
+
+	// Iterate over expectedAttrs, checking that:
+	// 1. Every attribute in expectedAttrs appears in collectedAttributes
+	// 2. All values in collectedAttributes match the regular expressions stored
+	//    in expectedAttrs.
+	for attribute, expectedPattern := range expectedAttrs {
+		if _, ok := collectedAttributes[attribute]; !ok {
+			t.Errorf("Missing expected attribute %q on metric %q. Found attributes: %v", attribute, metricName, keys(collectedAttributes))
+			continue
+		}
+
+		for actualValue, _ := range collectedAttributes[attribute] {
+			match, matchErr := regexp.MatchString(fmt.Sprintf("^(?:%s)$", expectedPattern), actualValue)
+			if matchErr != nil {
+				t.Errorf("Error parsing pattern. metric=%s, attribute=%s, pattern=%s, err=%v",
+					metricName,
+					attribute,
+					expectedPattern,
+					matchErr,
+				)
+			} else if !match {
+				t.Errorf("Attribute value does not match pattern. metric=%s, attribute=%s, pattern=%s, value=%s",
+					metricName,
+					attribute,
+					expectedPattern,
+					actualValue,
+				)
+			}
+		}
+	}
+}
+
+func expectMetricsLookRight(t *testing.T, metrics pmetric.Metrics, expectedMetrics map[string]ExpectedMetric) {
+	seen := make(map[string]bool)
+
+	// todo: it's kinda weird that we need to iterate over all Resourcemetrics in order to pick up all the expected metrics. I didn't think that would be the case and that it should be enough to just iterate over all the ScopeMetrics.
+	resourceMetrics := metrics.ResourceMetrics()
+	for i := 0; i < resourceMetrics.Len(); i++ {
+		scopeMetrics := resourceMetrics.At(i).ScopeMetrics()
+		for i := 0; i < scopeMetrics.Len(); i++ {
+			metrics := scopeMetrics.At(i).Metrics()
+
+			for j := 0; j < metrics.Len(); j++ {
+				metric := metrics.At(j)
+
+				expectation, ok := expectedMetrics[metric.Name()]
+				if !ok {
+					// TODO: Probably change to t.Logf once we've figured out why
+					// system.disk.average_operation_time is appearing and whether we
+					// should add any assertions about it.
+					t.Errorf("Unexpected metric with name %q", metric.Name())
+					continue
+				}
+
+				if seen[metric.Name()] {
+					t.Errorf("Saw more than one entry for %q", metric.Name())
+				}
+				seen[metric.Name()] = true
+
+				if got, want := metric.DataType().String(), expectation.Kind; got != want {
+					t.Errorf("Metric %q had unexpected DataType() %q. want %q", metric.Name(), got, want)
+				}
+
+				var dataPoints pmetric.NumberDataPointSlice
+				switch metric.DataType() {
+				case pmetric.MetricDataTypeGauge:
+					dataPoints = metric.Gauge().DataPoints()
+				case pmetric.MetricDataTypeSum:
+					dataPoints = metric.Sum().DataPoints()
+				default:
+					t.Errorf("Unimplemented handling for metric type %v", metric.DataType())
+				}
+
+				expectAttributesMatch(t, dataPoints, expectation.Attributes, metric.Name())
+			}
+		}
+	}
+
+	for expectedName, _ := range expectedMetrics {
+		if !seen[expectedName] {
+			t.Errorf("Never saw metric with name %q", expectedName)
+		}
+	}
 }

3e729264c9a862bb60bbebf8a226882865453944
suffian khan <sufkha@google.com>
2022-10-10 19:14:33 +0000

    rename test files

--- receiver/nvmlreceiver/client_gpu_test.go
+++ receiver/nvmlreceiver/client_gpu_test.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build with_gpu
-// +build with_gpu
+//go:build gpu
+// +build gpu
 
 package nvmlreceiver
 

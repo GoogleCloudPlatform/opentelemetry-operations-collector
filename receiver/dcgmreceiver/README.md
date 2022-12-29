fd2311270b6b78df87e351b09f0ce26a11260088
suffian khan <sufkha@google.com>
2022-12-29 13:44:14 +0000

    add dcgm receiver

---
+++ receiver/dcgmreceiver/README.md
@@ -0,0 +1,3 @@
+# Nvidia DCGM Receiver
+
+This receiver uses Nvidia's DCGM [Go API](https://github.com/NVIDIA/go-dcgm) to collect Nvidia GPU metrics.
---
+++ receiver/dcgmreceiver/util.go
@@ -0,0 +1,75 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dcgmreceiver
+
+import (
+	"unsafe"
+
+	"github.com/NVIDIA/go-dcgm/pkg/dcgm"
+)
+
+func (m *dcgmMetric) setFloat64(val float64) {
+	*(*float64)(unsafe.Pointer(&m.value[0])) = val
+}
+
+func (m *dcgmMetric) asFloat64() float64 {
+	return *(*float64)(unsafe.Pointer(&m.value[0]))
+}
+
+func (m *dcgmMetric) setInt64(val int64) {
+	*(*int64)(unsafe.Pointer(&m.value[0])) = val
+}
+
+func (m *dcgmMetric) asInt64() int64 {
+	return *(*int64)(unsafe.Pointer(&m.value[0]))
+}
+
+func isValidValue(fieldValue dcgm.FieldValue_v1) bool {
+	switch fieldValue.FieldType {
+	case 'd':
+		switch v := fieldValue.Float64(); v {
+		case dcgm.DCGM_FT_FP64_BLANK:
+			return false
+		case dcgm.DCGM_FT_FP64_NOT_FOUND:
+			return false
+		case dcgm.DCGM_FT_FP64_NOT_SUPPORTED:
+			return false
+		case dcgm.DCGM_FT_FP64_NOT_PERMISSIONED:
+			return false
+		}
+
+	case 'i':
+		switch v := fieldValue.Int64(); v {
+		case dcgm.DCGM_FT_INT32_BLANK:
+			return false
+		case dcgm.DCGM_FT_INT32_NOT_FOUND:
+			return false
+		case dcgm.DCGM_FT_INT32_NOT_SUPPORTED:
+			return false
+		case dcgm.DCGM_FT_INT32_NOT_PERMISSIONED:
+			return false
+		case dcgm.DCGM_FT_INT64_BLANK:
+			return false
+		case dcgm.DCGM_FT_INT64_NOT_FOUND:
+			return false
+		case dcgm.DCGM_FT_INT64_NOT_SUPPORTED:
+			return false
+		case dcgm.DCGM_FT_INT64_NOT_PERMISSIONED:
+			return false
+		}
+	}
+
+	return true
+}

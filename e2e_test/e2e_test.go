83d1c431c36d41f108820798bd8df0757262fe90
avilevy <avilevy@google.com>
2024-10-04 18:00:36 +0000

    Making GCP project an environment variable

--- e2e_test/e2e_test.go
+++ e2e_test/e2e_test.go
@@ -5,15 +5,20 @@ package e2e_test
 import (
 	"context"
 	"github.com/GoogleCloudPlatform/opentelemetry-operations-collector/e2e_test/gce"
+	"os"
 	"testing"
 )
 
 func TestGcloud(t *testing.T) {
+	projectName := os.Getenv("proj")
+	if projectName == "" {
+		t.Fatal("No proj enviroment variable found")
+	}
 	ctx := context.Background()
 	logger := gce.SetupLogger(t)
 	vmOptions := gce.VMOptions{
 		ImageSpec: "cos-cloud:cos-stable",
-		Project:   "cloud-ops-agents-artifacts-dev",
+		Project:   projectName,
 		Zone:      "us-central1-a",
 	}
 	vm, err := gce.CreateInstance(ctx, logger.ToFile("VM_initialization.txt"), vmOptions)

111bc54d5c6f3ffdfe3c320accdf904aa676a7ca
braydonk <braydonk@google.com>
2025-02-11 21:22:37 +0000

    remove automatic default config from container

--- google-otel/start_otelcol.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-CONFIG_LOCATION="/etc/otelcol-google/config-standard.yaml"
-if [ ! -z "$KUBERNETES_SERVICE_HOST" ]; then
-  CONFIG_LOCATION="/etc/otelcol-google/config-k8s.yaml"
-fi
-
-cp $CONFIG_LOCATION /etc/otelcol-google/config.yaml
-/otelcol-google $@

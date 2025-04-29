a9dd24b53416a4414b5daa04f7637ada4ff08b95
Ida Hou <xuechun@google.com>
2025-04-29 18:15:09 +0000

    completed google secret manager imple

--- provider/googlesecretsprovider/provider.go
+++ provider/googlesecretsprovider/provider.go
@@ -10,12 +10,17 @@ import (
 	"fmt"
 	"strings"
 
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	gax "github.com/googleapis/gax-go/v2"
+	"github.com/googleapis/gax-go/v2/apierror"
 	"go.opentelemetry.io/collector/confmap"
-	"go.uber.org/zap"
+
+	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
 )
 
 type secretsManagerClient interface {
-	GetSecretValue(ctx context.Context) error
+	AccessSecretVersion(ctx context.Context, req *secretmanagerpb.AccessSecretVersionRequest, opts ...gax.CallOption) (*secretmanagerpb.AccessSecretVersionResponse, error)
+	Close() error
 }
 
 const (
@@ -24,23 +29,46 @@ const (
 
 type provider struct {
 	client secretsManagerClient
-	logger *zap.Logger
 }
 
 func NewFactory() confmap.ProviderFactory {
-	return confmap.NewProviderFactory(newWithSettings)
+	return confmap.NewProviderFactory(newProvider)
 }
 
-func newWithSettings(ps confmap.ProviderSettings) confmap.Provider {
-	return &provider{client: nil, logger: ps.Logger}
+func newProvider(confmap.ProviderSettings) confmap.Provider {
+	return &provider{client: nil}
 }
 
-func (provider *provider) Retrieve(_ context.Context, uri string, _ confmap.WatcherFunc) (*confmap.Retrieved, error) {
+func (p *provider) Retrieve(ctx context.Context, uri string, _ confmap.WatcherFunc) (*confmap.Retrieved, error) {
 	if !strings.HasPrefix(uri, schemeName+":") {
 		return nil, fmt.Errorf("%q uri is not supported by %q provider", uri, schemeName)
 	}
+	secretName := strings.TrimPrefix(uri, schemeName+":")
 
-	return confmap.NewRetrieved("/data")
+	if p.client == nil {
+		client, err := secretmanager.NewClient(ctx)
+		if err != nil {
+
+			return nil, fmt.Errorf("failed to create a Google secret manager client: %w", err)
+		}
+		defer client.Close()
+		p.client = client
+	}
+	req := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: secretName,
+	}
+	resp, err := p.client.AccessSecretVersion(ctx, req)
+
+	if err != nil {
+		var apiErr *apierror.APIError
+		apiErr, ok := apierror.FromError(err)
+		if !ok {
+			return nil, fmt.Errorf("failed to access secret version: %w", err)
+		}
+		return nil, fmt.Errorf("failed to access secret version: %v", apiErr.Error())
+	}
+
+	return confmap.NewRetrieved(string(resp.GetPayload().GetData()))
 }
 
 func (*provider) Scheme() string {

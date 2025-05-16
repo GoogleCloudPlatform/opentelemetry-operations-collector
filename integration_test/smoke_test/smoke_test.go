b92288a243d7c02486e0d2cd06e3ce15ecbc2b3b
Martijn van Schaardenburg <martijnvs@google.com>
2025-05-16 17:58:34 +0000

    dummy commit

--- integration_test/smoke_test/smoke_test.go
+++ integration_test/smoke_test/smoke_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Env variables:
+// Env variables (TODO: comment)
 // - ZONES
 // - IMAGE_SPECS
 // - PROJECT

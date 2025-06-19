9ef32451084fc4506289b0d3369a66776dfc6bb6
avilevy <avilevy@google.com>
2025-06-19 18:36:36 +0000

    Fixing docker build command

--- custom-build/build.sh
+++ custom-build/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --build-arg CUSTOM_COMPONENTS=${_LOUHI_CUSTOM_COMPONENTS} --output=type=oci,dest=$KOKORO_ARTIFACTS_DIR/container.tar .
+docker build --build-arg CUSTOM_COMPONENTS=${_LOUHI_CUSTOM_COMPONENTS} --output=type=oci,dest=$KOKORO_ARTIFACTS_DIR/container.tar --file git/otelcol-google/custom-build/Dockerfile .

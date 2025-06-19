9169f5585663e16a63ea99e1f4b477a200604546
Edward Li <edwardjxli@google.com>
2025-06-19 18:32:58 +0000

    test 7

---
+++ custom-build/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+docker build --build-arg CUSTOM_COMPONENTS=${CUSTOM_COMPONENTS} --output=type=oci,dest=$KOKORO_ARTIFACTS_DIR/container.tar --file git/otelcol-google/custom-build/Dockerfile.build

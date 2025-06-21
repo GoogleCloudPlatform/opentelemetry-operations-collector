ef3a97d2cf6bd26c108f83bdef0cc8e28a7c2889
XuechunHou <xuechun@google.com>
2025-06-21 03:18:47 +0000

    export secret to env var

--- aoss/build_images_with_aoss.sh
+++ aoss/build_images_with_aoss.sh
@@ -26,8 +26,9 @@ gcloud version
 # sudo apt-get install -y google-cloud-cli
 # echo "Verifying gcloud installation..."
 # gcloud version
-gcloud secrets versions access 1 --secret=aoss-ar-repos-authentication-credential --project=372639168729 > $HOME/.netrc
-echo $HOME/.netrc
 
-docker build --build-arg AOSS_AUTH_TOKEN=${GOOGLE_APPLICATION_CREDENTIALS} --output=type=oci,dest=$KOKORO_ARTIFACTS_DIR/container.tar --file git/otelcol-google/aoss/Dockerfile.build .
+
+export AOSS_AUTH_TOKEN="$(gcloud secrets versions access 1 --secret=aoss-ar-repos-authentication-credential --project=372639168729)"
+echo $AOSS_AUTH_TOKEN
+docker build --build-arg AOSS_AUTH_TOKEN=${AOSS_AUTH_TOKEN} --output=type=oci,dest=$KOKORO_ARTIFACTS_DIR/container.tar --file git/otelcol-google/aoss/Dockerfile.build .
 

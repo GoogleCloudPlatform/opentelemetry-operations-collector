a89808ca9be101683b75ba6d50dee15b3e9dd504
XuechunHou <xuechun@google.com>
2025-06-25 17:45:04 +0000

    refactored make file template

---
+++ google-built-opentelemetry-collector/pull_modules_from_ar_repos.mk
@@ -0,0 +1,18 @@
+include ./Makefile
+
+
+AR_AUTH_BIN = $(TOOLS_DIR)/auth
+
+$(AR_AUTH_BIN): $(GO_BIN)
+	@{ \
+	set -e ;\
+	mkdir -p $(TOOLS_DIR) ;\
+	echo "Installing AR Auth tools at $(TOOLS_DIR)" ;\
+	GOBIN=$(TOOLS_DIR) CGO_ENABLED=0 $(GO_BIN) install -trimpath -ldflags="-s -w" github.com/GoogleCloudPlatform/artifact-registry-go-tools/cmd/auth@v0.4.0 ;\
+	} 
+
+.PHONY: aoss-build
+aoss-build: $(AR_AUTH_BIN)
+	$(AR_AUTH_BIN) add-locations --locations=us && \
+	$(AR_AUTH_BIN) refresh && \
+	$(MAKE) build

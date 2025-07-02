f01afeb21b10a33d410ffbf4dd7cddac45d30b31
Braydon Kains <93549768+braydonk@users.noreply.github.com>
2025-07-02 13:50:53 -0400

    Change distro version to 0.129.0 (#362)

--- google-built-opentelemetry-collector/docs/examples/deployment/cos/run.sh
+++ google-built-opentelemetry-collector/docs/examples/deployment/cos/run.sh
@@ -2,5 +2,5 @@ docker run -d \
     --network otel \
     --name opentelemetry-collector \
     -v /etc/config:/etc/config \
-    us-docker.pkg.dev/cloud-ops-agents-artifacts/google-cloud-opentelemetry-collector/otelcol-google:0.128.0 \
+    us-docker.pkg.dev/cloud-ops-agents-artifacts/google-cloud-opentelemetry-collector/otelcol-google:0.129.0 \
     --config=/etc/config/config.yaml

9341139cfda416816a2da4e718c32ae3c0d2df3c
avilevy <avilevy@google.com>
2025-09-22 21:28:13 +0000

    Clean up

--- google-built-opentelemetry-collector/pull_modules_from_ar_repos.mk
+++
@@ -1,7 +0,0 @@
-include ./Makefile
-
-USE_GO_PROXY ?= https://us-go.pkg.dev/access-aoss/assuredoss-go
-
-.PHONY: aoss-build
-aoss-build:
-	$(MAKE) USE_GO_PROXY=https://us-go.pkg.dev/access-aoss/assuredoss-go build

4ed1c47a8091cf4b7eec189254baa24fec9e63d0
braydonk <braydonk@google.com>
2026-01-26 15:35:41 +0000

    GBOC: Fix missed generate
    This PR fixes a generate command that I missed when trying to update to
    GBOC v0.144.0

--- google-built-opentelemetry-collector/docs/examples/deployment/cos/run.sh
+++ google-built-opentelemetry-collector/docs/examples/deployment/cos/run.sh
@@ -2,5 +2,5 @@ docker run -d \
     --network otel \
     --name opentelemetry-collector \
     -v /etc/config:/etc/config \
-    us-docker.pkg.dev/cloud-ops-agents-artifacts/google-cloud-opentelemetry-collector/otelcol-google:0.143.0 \
+    us-docker.pkg.dev/cloud-ops-agents-artifacts/google-cloud-opentelemetry-collector/otelcol-google:0.144.0 \
     --config=/etc/config/config.yaml

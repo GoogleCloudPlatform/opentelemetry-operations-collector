2862b155ba3acaeb8934e6ac5ba427a27b0e45d9
XuechunHou <xuechun@google.com>
2026-06-12 20:01:35 +0000

    test: update isRHEL9 to recognize AlmaLinux 9

--- integration_test/gce-testing-internal/gce/gce_testing.go
+++ integration_test/gce-testing-internal/gce/gce_testing.go
@@ -1593,11 +1593,11 @@ func IsCentOS(imageSpec string) bool {
 }
 
 func IsRHEL(imageSpec string) bool {
-	return strings.HasPrefix(imageSpec, "rhel-") || strings.Contains(imageSpec, "almalinux-")
+	return strings.HasPrefix(imageSpec, "rhel-")
 }
 
 func isRHEL9(imageSpec string) bool {
-	return strings.Contains(imageSpec, "rhel-9") || strings.Contains(imageSpec, "rocky-linux-9")
+	return strings.Contains(imageSpec, "rhel-9") || strings.Contains(imageSpec, "rocky-linux-9") || strings.Contains(imageSpec, "almalinux-9")
 }
 
 func isRHEL7SAPHA(imageSpec string) bool {
@@ -1609,7 +1609,7 @@ func IsDLVMImage(imageSpec string) bool {
 }
 
 func IsRocky(imageSpec string) bool {
-	return strings.Contains(imageSpec, "rocky-linux-")
+	return strings.Contains(imageSpec, "rocky-linux-") || strings.Contains(imageSpec, "almalinux-")
 }
 
 func IsRpm(imageSpec string) bool {
